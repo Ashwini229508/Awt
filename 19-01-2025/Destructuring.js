d191e0d5b611135923023c3893d2f51fdaae664f
Ashwini229508 <gouraashwini55@gmail.com>
2025-02-21 15:10:19 +0530

    Add files via upload

---
+++ 19-01-2025/Destructuring.js
@@ -0,0 +1,22 @@
+//class
+const user = {
+    id: 1,
+    profile: {
+      name: 'Anu',
+      age: 25,
+    },
+  };
+  
+  const {
+    profile: { name, age },
+  } = user;
+  console.log(name); 
+  console.log(age);  
+  
+  //Array
+const points = [1, [2, 3], 4];
+const [a, [b, c], d] = points;
+console.log(a); 
+console.log(b); 
+console.log(c); 
+console.log(d); 
